216fa2d910129a7160d02ab64bc9cc5b4c4513be
Emil <emiles1997@gmail.com>
2022-10-11 15:05:49 +0300

    первый коммит

---
+++ src/test/java/com/example/workwithmap/WorkWithMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workwithmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkWithMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
